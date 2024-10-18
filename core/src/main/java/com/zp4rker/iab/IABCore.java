321d82969d618aeae02c78560a93928e633c2b4f
zp4rker <iam@zp4rker.com>
2024-10-18 17:23:56 +1100

    remove database test

--- core/src/main/java/com/zp4rker/iab/IABCore.java
+++ core/src/main/java/com/zp4rker/iab/IABCore.java
@@ -1,11 +1,9 @@
 package com.zp4rker.iab;
 
-import com.zp4rker.iab.api.*;
 import com.zp4rker.iab.db.DBManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
-import java.util.UUID;
 import java.util.logging.Logger;
 
 public class IABCore extends JavaPlugin {
@@ -24,8 +22,6 @@ public class IABCore extends JavaPlugin {
 
         connectDatabase();
         createTables();
-
-        testDatabase();
     }
 
     @Override
@@ -52,7 +48,6 @@ public class IABCore extends JavaPlugin {
             LOGGER.info("Connected to database");
         } catch (SQLException e) {
             LOGGER.severe("Unable to connect to database! Shutting down...");
-            e.printStackTrace();
             getServer().getPluginManager().disablePlugin(this);
         }
     }
@@ -63,18 +58,7 @@ public class IABCore extends JavaPlugin {
             LOGGER.info("Created tables");
         } catch (SQLException e) {
             LOGGER.severe("Unable to create tables! Shutting down...");
-            e.printStackTrace();
             getServer().getPluginManager().disablePlugin(this);
         }
-    }
-
-    private void testDatabase() {
-        Planet planet = new Planet("Origin", new Loc(0, 0, 0), 1000, 1000);
-        Explorer explorer = new Explorer(UUID.randomUUID(), "zp4rker", 0, 0, 0, 0);
-        Spaceship spaceship = new Spaceship("The Explorer", explorer, new PlanetaryLocation(planet, 0, 0, 0));
-
-        LOGGER.info("Planet save and read: " + (planet.save() == planet.inDatabase()));
-        LOGGER.info("Explorer save and read: " + (explorer.save() == explorer.inDatabase()));
-        LOGGER.info("Spaceship save and read: " + (spaceship.save() == spaceship.inDatabase()));
     }
 }
