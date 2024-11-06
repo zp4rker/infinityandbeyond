9cc3f17831a30afe3e242fbfa0c0efc7b113d7cb
zp4rker <iam@zp4rker.com>
2024-11-07 08:58:24 +1100

    add draw cube and draw prism commands

--- src/main/kotlin/com/zp4rker/iab/commands/Debug.kt
+++ src/main/kotlin/com/zp4rker/iab/commands/Debug.kt
@@ -2,6 +2,7 @@ package com.zp4rker.iab.commands
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Subcommand
 import com.zp4rker.bukkot.extensions.runTaskTimer
 import com.zp4rker.bukkot.extensions.toTicks
@@ -17,44 +18,76 @@ import java.util.concurrent.TimeUnit
 
 @CommandAlias("debug")
 object Debug : BaseCommand() {
-    @Subcommand("drawborder")
-    fun drawBorder(player: Player) {
-        fun drawLine(start: Location, end: Location, vec: Vector) {
-            for (i in 0..(start.distance(end).toInt() * 5)) {
-                val loc = start.clone().add(vec.clone().multiply(i))
-                Particle.DUST.builder().apply {
-                    color(Color.RED)
-                    location(loc)
-                    receivers(10)
-                }.spawn()
-            }
-        }
-
-        fun drawSquare(location: Location) {
-            location.clone().apply {
-                drawLine(this, clone().add(0.0, 0.0, 1.0), Vector(0.0, 0.0, 0.2))
-                drawLine(this, clone().add(1.0, 0.0, 0.0), Vector(0.2, 0.0, 0.0))
-                drawLine(add(1.0, 0.0, 0.0), clone().add(0.0, 0.0, 1.0), Vector(0.0, 0.0, 0.2))
-                drawLine(add(-1.0, 0.0, 1.0), clone().add(1.0, 0.0, 0.0), Vector(0.2, 0.0, 0.0))
-            }
-        }
-
-        val loc = player.location.toBlockLocation().add(2.0, 0.0, 0.0)
+    @Subcommand("draw cube")
+    fun drawCube(player: Player, @Default("5") size: Int) {
+        val start = player.location.toBlockLocation().add(2.0, 0.0, 0.0)
+        val end = start.clone().add(size.toDouble(), size.toDouble(), size.toDouble())
         val task = IAB.runTaskTimer(period = TimeUnit.MILLISECONDS.toTicks(125)) {
-            drawSquare(loc)
-            for (x in 0..1) {
-                for (z in 0..1) {
-                    val start = loc.clone().add(x.toDouble(), 0.0, z.toDouble())
-                    val end = start.clone().add(0.0, 5.0, 0.0)
-                    val vec = Vector(0.0, 0.2, 0.0)
-                    drawLine(start, end, vec)
-                }
-            }
-            drawSquare(loc.clone().add(0.0, 5.0, 0.0))
+            drawBox(start, end)
         }
 
-        player.expect<PlayerMoveEvent>(IAB, predicate = { it.to.distance(loc) <= 1.0 }) {
+        player.expect<PlayerMoveEvent>(IAB, predicate = { e ->
+            e.to.x.let { it >= start.x && it <= end.x } &&
+                    e.to.y.let { it >= start.y && it <= end.y } &&
+                    e.to.z.let { it >= start.z && it <= end.z }
+        }) {
             task.cancel()
+        }
+    }
+
+    @Subcommand("draw prism")
+    fun drawPrism(player: Player, @Default("5") width: Int, @Default("3") height: Int) {
+        val start = player.location.toBlockLocation().add(2.0, 0.0, 0.0)
+        val end = start.clone().add(width.toDouble(), height.toDouble(), width.toDouble())
+        val task = IAB.runTaskTimer(period = TimeUnit.MILLISECONDS.toTicks(125)) {
+            drawBox(start, end)
+        }
+
+        player.expect<PlayerMoveEvent>(IAB, predicate = { e ->
+            e.to.x.let { it >= start.x && it <= end.x } &&
+                    e.to.y.let { it >= start.y && it <= end.y } &&
+                    e.to.z.let { it >= start.z && it <= end.z }
+        }) {
+            task.cancel()
+        }
+    }
+
+    private fun drawBox(start: Location, end: Location) {
+        drawLine(start, end.clone().set(start.x, start.y, end.z), Vector(0.0, 0.0, 0.1))
+        drawLine(start, end.clone().set(end.x, start.y, start.z), Vector(0.1, 0.0, 0.0))
+        drawLine(start, end.clone().set(start.x, end.y, start.z), Vector(0.0, 0.1, 0.0))
+
+        drawLine(end, start.clone().set(end.x, end.y, start.z), Vector(0.0, 0.0, -0.1))
+        drawLine(end, start.clone().set(start.x, end.y, end.z), Vector(-0.1, 0.0, 0.0))
+        drawLine(end, start.clone().set(end.x, start.y, end.z), Vector(0.0, -0.1, 0.0))
+
+        drawLine(start.clone().apply { x = end.x }, end.clone().set(end.x, end.y, start.z), Vector(0.0, 0.1, 0.0))
+        drawLine(start.clone().apply { x = end.x }, end.clone().set(end.x, start.y, end.z), Vector(0.0, 0.0, 0.1))
+
+        drawLine(start.clone().apply { z = end.z }, end.clone().set(start.x, end.y, end.z), Vector(0.0, 0.1, 0.0))
+        drawLine(start.clone().apply { z = end.z }, end.clone().set(end.x, start.y, end.z), Vector(0.1, 0.0, 0.0))
+
+        drawLine(start.clone().apply { y = end.y }, end.clone().set(end.x, end.y, start.z), Vector(0.1, 0.0, 0.0))
+        drawLine(start.clone().apply { y = end.y }, end.clone().set(start.x, end.y, end.z), Vector(0.0, 0.0, 0.1))
+    }
+
+    private fun drawLine(start: Location, end: Location, vec: Vector = Vector(0.0, 0.1, 0.0)) {
+        for (i in 0..(start.distance(end).toInt() * 10)) {
+            val loc = start.clone().add(vec.clone().multiply(i))
+            Particle.DUST.builder().apply {
+                color(Color.RED)
+                location(loc)
+                receivers(10 + start.distance(end).toInt())
+            }.spawn()
+        }
+    }
+
+    private fun drawSquare(location: Location) {
+        location.clone().apply {
+            drawLine(this, clone().add(0.0, 0.0, 1.0), Vector(0.0, 0.0, 0.2))
+            drawLine(this, clone().add(1.0, 0.0, 0.0), Vector(0.2, 0.0, 0.0))
+            drawLine(add(1.0, 0.0, 0.0), clone().add(0.0, 0.0, 1.0), Vector(0.0, 0.0, 0.2))
+            drawLine(add(-1.0, 0.0, 1.0), clone().add(1.0, 0.0, 0.0), Vector(0.2, 0.0, 0.0))
         }
     }
 }
